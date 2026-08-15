73dd377caf90e73c257ad5df7d823b8631b2c2b0
Apieceoftears <linm99685@gmail.com>
2026-08-14 23:35:33 -0400

    Update GLFW window

--- main.cpp
+++ main.cpp
@@ -13,5 +13,19 @@ int main() {
 
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "GameDevelopementProject", NULL, NULL);
 
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+			glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
